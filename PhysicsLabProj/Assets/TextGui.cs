c542057778caeb54183ec1912eb58f24a338c669
Quinn Levine <Qlevine@uci.edu>
2015-05-01 12:24:21 -0700

    Created a somewhat working input field that only takes floats.

---
+++ PhysicsLabProj/Assets/TextGui.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class TextGui : MonoBehaviour {
+
+	private float GuiText;
+	private InputField InputF;
+	private string new_str = "1.234";
+	void Start (){
+		InputF = GameObject.Find ("InputField").GetComponent<InputField>();
+
+	}
+	void LateUpdate(){
+
+
+		if(Input.GetKey(KeyCode.Return)){
+		new_str = InputF.text;
+		
+		float float_num;
+		
+		if (float.TryParse (new_str,out float_num)) {
+			GuiText = float_num;
+			}
+	}
+
+	}
+}
