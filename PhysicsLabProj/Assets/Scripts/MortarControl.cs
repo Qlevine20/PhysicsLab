691015c9a65413a07d488ad55191d044ecf93cba
Quinn Levine <Qlevine@uci.edu>
2015-05-02 12:25:18 -0700

    The object now rotates in the y and z direction.  There is a prefab as well that can be easily used for any other user inputs that we need which should make other inputs easier.

---
+++ PhysicsLabProj/Assets/Scripts/MortarControl.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI; //required when using UI elements
+
+public class MortarControl : MonoBehaviour {
+
+	public float angle_z = 45.0f;
+	public float angle_y = 0.0f;
+	public Vector3 start_force;
+	public InputField input_y;
+	public InputField input_z;
+
+
+	//for tracking previous inputs
+	private string prev_y;
+	private string prev_z;
+
+	// Use this for initialization
+	void Start () {
+		prev_y = input_y.text;
+		prev_z = input_z.text;
+	}
+
+	//getting the data from inside the textbox
+
+	void LateUpdate () {
+		if ((input_y.text != prev_y || input_z.text != prev_z) && Input.GetKey(KeyCode.Return)) {
+			angle_y = (input_y.gameObject.GetComponent<TextGui>().GuiText);
+			angle_z = (input_z.gameObject.GetComponent<TextGui>().GuiText);
+			transform.Rotate (new Vector3 (0.0f, angle_y, angle_z));
+			prev_y = input_y.text;
+			prev_z = input_z.text;
+		}
+	}
+
+
+}
---
+++ PhysicsLabProj/Assets/Scripts/TextGui.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class TextGui : MonoBehaviour {
+
+	public float GuiText;
+	private InputField InputF;
+	private string new_str = "1.234";
+	void Start (){
+		InputF = this.gameObject.GetComponent<InputField>();
+
+	}
+	void Update(){
+
+		if (InputF.isFocused) {
+				new_str = InputF.text;
+		
+				float float_num;
+		
+				if (float.TryParse (new_str, out float_num)) {
+					GuiText = float_num;
+			}
+		}
+	}
+}
