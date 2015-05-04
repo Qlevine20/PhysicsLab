937cca1b7ce197ff3adb24bcc792ef135b74f742
zhhart <zachary.hart825@yahoo.com>
2015-05-04 12:38:10 -0700

    Didn't push correctly had to make changes again.  Still not working as well as before.

--- PhysicsLabProj/Assets/Scripts/MortarControl.cs
+++ PhysicsLabProj/Assets/Scripts/MortarControl.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI; //required when using UI elements
 public class MortarControl : MonoBehaviour {
 
 	public float angle_z = 45.0f;
-	public float angle_y = 0.1f;
+	public float angle_y = 0.0f;
 	public float max_y = 90.0f;
 	public float max_z = 90.0f;
 	public float min_y = 0.0f;
@@ -25,7 +25,7 @@ public class MortarControl : MonoBehaviour {
 		start_position = this.transform.position;
 		prev_y = input_y.text;
 		prev_z = input_z.text;
-		transform.RotateAround (this.transform.position, Vector3.zero, angle_z);
+		transform.RotateAround (start_position, Vector3.zero, 90-angle_z);
 	}
 
 	//getting the data from inside the textbox
@@ -35,10 +35,10 @@ public class MortarControl : MonoBehaviour {
 
 			angle_y = (input_y.gameObject.GetComponent<TextGui>().GuiText);
 			angle_z = (input_z.gameObject.GetComponent<TextGui>().GuiText);
-			if (angle_y < max_y && angle_z < max_z && angle_y >= min_y && angle_z >= min_z){
+			if (angle_y <= max_y && angle_z <= max_z && angle_y >= min_y && angle_z >= min_z){
 
 			this.transform.rotation = Quaternion.identity;
-			transform.RotateAround (start_position, Vector3.left, angle_z);
+				transform.RotateAround (start_position, Vector3.zero, 90-angle_z);
 			prev_y = input_y.text;
 			prev_z = input_z.text;
 			}
