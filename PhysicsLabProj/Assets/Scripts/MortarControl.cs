f7a764ecd617b517a5fd1c9b22311086cbb9dcf6
Quinn Levine <Qlevine@uci.edu>
2015-05-06 21:41:09 -0700

    Added in a Velocity Text Field, displayed the distance to each target in a text box, if there are more than one, and allowed the user to change the velocity.

--- PhysicsLabProj/Assets/Scripts/MortarControl.cs
+++ PhysicsLabProj/Assets/Scripts/MortarControl.cs
@@ -18,20 +18,38 @@ public class MortarControl : MonoBehaviour {
     public Rigidbody projectile;
     public float power;
     public Vector3 launch_force;
+    public Text distance;
+    public GameObject[] Targets;
 
 
     //for tracking previous inputs
     private string prev_y;
 	private string prev_z;
+    private float prev_power;
 
 	// Use this for initialization
 	void Start () {
+        power = GameObject.Find("Velocity").GetComponentInChildren<TextGui>().GuiText;
+        prev_power = power;
+        distance = GameObject.Find("DistanceUI").GetComponentInChildren<Text>();
         shot_remains = false;
 		start_position = this.transform.position;
 		prev_y = input_y.text;
 		prev_z = input_z.text;
 		transform.RotateAround (start_position, Vector3.forward, 90-angle_z);
+        Targets = GameObject.FindGameObjectsWithTag("Target");
+        ShowTargetInformation();
+
 	}
+
+    void ShowTargetInformation()
+    {
+        foreach (GameObject target in Targets)
+        {
+            distance.text+= string.Format("Distance to Target: {0} \n",this.transform.position - target.transform.position );
+        }
+
+    }
 
 
     void FireProjectile()
@@ -56,16 +74,18 @@ public class MortarControl : MonoBehaviour {
 	//getting the data from inside the textbox
 
 	void LateUpdate () {
-		if ((input_y.text != prev_y || input_z.text != prev_z) && Input.GetKey(KeyCode.Return)) {
+        power = GameObject.Find("Velocity").GetComponentInChildren<TextGui>().GuiText;
+        if ((input_y.text != prev_y || input_z.text != prev_z || power!= prev_power) && Input.GetKey(KeyCode.Return)) {
 
 			angle_y = (input_y.gameObject.GetComponent<TextGui>().GuiText);
 			angle_z = (input_z.gameObject.GetComponent<TextGui>().GuiText);
-			if (angle_y <= max_y && angle_z <= max_z && angle_y >= min_y && angle_z >= min_z){
+            if (angle_y <= max_y && angle_z <= max_z && angle_y >= min_y && angle_z >= min_z){
 
 			    this.transform.rotation = Quaternion.identity;
 				transform.RotateAround (start_position, Vector3.forward, 90-angle_z);
 			    prev_y = input_y.text;
 			    prev_z = input_z.text;
+                prev_power = power;
 			}
 			else
 			{
--- PhysicsLabProj/Assets/Scripts/TextGui.cs
+++ PhysicsLabProj/Assets/Scripts/TextGui.cs
@@ -6,7 +6,9 @@ public class TextGui : MonoBehaviour {
 	public float GuiText;
 	private InputField InputF;
 	private string new_str = "1.234";
+    public float _default;
 	void Start (){
+        GuiText = _default;
 		InputF = this.gameObject.GetComponent<InputField>();
 
 	}
@@ -16,10 +18,15 @@ public class TextGui : MonoBehaviour {
 				new_str = InputF.text;
 		
 				float float_num;
-		
-				if (float.TryParse (new_str, out float_num)) {
-					GuiText = float_num;
+
+            if (float.TryParse(new_str, out float_num))
+            {
+                GuiText = float_num;
             }
-		}
+            else
+            {
+                GuiText = _default;
+            }
+        }
 	}
 }
