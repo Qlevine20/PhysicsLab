3092bc86081c95fc35370ebb92eac9b5232d97a1
Quinn Levine <Qlevine@uci.edu>
2015-05-11 15:57:31 -0700

    Changed the intro panel.  Added a text intro panel to explain what the player has to do.

--- PhysicsLabProj/Assets/Scripts/IntroScript.cs
+++ PhysicsLabProj/Assets/Scripts/IntroScript.cs
@@ -4,27 +4,20 @@ using System.Collections;
 public class IntroScript : MonoBehaviour {
 
     public GameObject but_text;
-    public static int but_count;
     public static bool intro_over;
 
 
     void Start()
     {
-        but_count = 4;
         intro_over = false;
     }
 
     public void Click()
     {
-        Destroy(but_text);
-        if (but_count == 1)
-        {
-            Destroy(GameObject.Find("IntroPanel"));
-            intro_over = true;
-            Destroy(gameObject);
-        }
-        but_count -= 1;
-        Destroy(gameObject);
+       Destroy(but_text);
+       Destroy(GameObject.Find("IntroPanel"));
+       intro_over = true;
+       Destroy(gameObject);
     }
 
 }
