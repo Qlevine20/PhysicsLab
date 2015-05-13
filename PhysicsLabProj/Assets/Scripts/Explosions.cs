76cc039ec274728fd2306947e126344208e8b4e0
Quinn Levine <Qlevine@uci.edu>
2015-05-13 01:19:33 -0700

    Fixed explosions again because gitignore ruins everything.   STUPID GITIGNORE

---
+++ PhysicsLabProj/Assets/Scripts/Explosions.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Explosions : MonoBehaviour {
+	public float time_to_die;
+	// Use this for initialization
+	void Awake () {
+		Destroy (this, time_to_die);
+	}
+}
