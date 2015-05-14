fc06092b7e8390ded4f13a2db30c104b66df8b2e
Quinn Levine <Qlevine@uci.edu>
2015-05-13 18:16:24 -0700

    Added First Person and moved initial camera to mortar, then deactivated that camera.  Also started setting up the initial contact between mortar and player.  When player is close to mortar the console will print "close enough" and "far enough" when they are not close.  Took out jump for first person controller.

---
+++ PhysicsLabProj/Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	private Vector3 distance_to_player;
+	private bool close_enough;
+	// Use this for initialization
+	void Start () {
+		distance_to_player = this.transform.position - GameObject.FindGameObjectWithTag ("Mortar").transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		distance_to_player = this.transform.position - GameObject.FindGameObjectWithTag ("Mortar").transform.position;
+
+		if (distance_to_player.sqrMagnitude < 30) {
+			print ("close enough");
+		} else 
+		{
+			print ("far enough");
+		}
+	}
+}
